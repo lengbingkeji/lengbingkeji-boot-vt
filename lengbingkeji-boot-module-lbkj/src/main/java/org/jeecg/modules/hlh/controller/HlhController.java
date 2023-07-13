fd500151a4f743a8895c55f5579036a9dc84091c
lengbingkeji <1517699467@qq.com>
2023-07-13 11:15:24 +0800

    lbkj 模块: 增加包 hlh

---
+++ lengbingkeji-boot-module-lbkj/src/main/java/org/jeecg/modules/hlh/controller/HlhController.java
@@ -0,0 +1,28 @@
+package org.jeecg.modules.hlh.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jeecg.common.api.vo.Result;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ClassName: HlhController
+ * Package: org.jeecg.modules.hlh.controller
+ * Description:
+ *
+ * @Author lengbing
+ * @Create 2023/7/13 10:28
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("/hth/wzlh")
+@Slf4j
+public class HlhController {
+    @GetMapping(value = "/wztolh")
+    public Result<String> wztolh(){
+        Result<String> result = new Result<String>();
+        result.setResult("hwz to hlh!");
+        return result;
+    }
+}
