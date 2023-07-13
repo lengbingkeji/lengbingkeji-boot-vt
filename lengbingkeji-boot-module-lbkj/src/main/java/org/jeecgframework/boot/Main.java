fe389c7b85e0bca2956561b6216d6de7339769a1
lengbingkeji <1517699467@qq.com>
2023-07-13 10:07:58 +0800

    添加 lengbingkeji-boot-module-lbkj 模块

---
+++ lengbingkeji-boot-module-lbkj/src/main/java/org/jeecgframework/boot/Main.java
@@ -0,0 +1,16 @@
+package org.jeecgframework.boot;
+
+/**
+ * ClassName: ${NAME}
+ * Package: org.jeecgframework.boot
+ * Description:
+ *
+ * @Author lengbing
+ * @Create ${DATE} ${TIME}
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
